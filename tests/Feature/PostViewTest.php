9252860d106f23037cca5dd681c2500fd74c966d
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-01-21 16:49:19 +0300

    refactor(test): refactor method names and add asserts

--- tests/Feature/PostViewTest.php
+++ tests/Feature/PostViewTest.php
@@ -6,34 +6,79 @@ namespace Tests\Feature;
 
 use Illuminate\Support\Str;
 use LaraPress\Post\Post;
+use Ramsey\Uuid\Uuid;
 use Tests\TestCase;
 
 class PostViewTest extends TestCase
 {
-    public function testForGettingPostById(): void
+    private function assertResponseData(array $data): void
     {
-        $post = Post::query()->first();
-        $response = $this->get("/api/posts/{$post->id}");
-        $response->assertStatus(200);
+        $this->assertArrayHasKey('id', $data);
+        $this->assertTrue(Uuid::isValid($data['id']));
+
+        $this->assertArrayHasKey('name', $data);
+        $this->assertArrayHasKey('slug', $data);
+
+        $this->assertArrayHasKey('status', $data);
+        $this->assertContains($data['status'], [Post::PUBLISH_STATUS, Post::DRAFT_STATUS, Post::PENDING_STATUS]);
+
+        $this->assertArrayHasKey('excerpt', $data);
+        $this->assertArrayHasKey('thumb', $data);
+        $this->assertArrayHasKey('createdAt', $data);
+        $this->assertArrayHasKey('updatedAt', $data);
     }
 
-    public function testForGettingPostBySlugWithDraftStatus(): void
+    /** @test */
+    public function it_can_get_post_by_id(): void
     {
-        $post = Post::currentStatus(Post::DRAFT_STATUS)->first();
+        $post = Post::query()->inRandomOrder()->limit(1)->first();
         $this->assertNotNull($post);
+
+        $response = $this->get("/api/posts/{$post->id}");
+        $response->assertStatus(200);
+
+        $data = $response->json('data');
+        $this->assertResponseData($data);
+    }
+
+    /** @test */
+    public function it_returns_404_when_getting_draft_post_by_slug(): void
+    {
+        $post = Post::currentStatus(Post::DRAFT_STATUS)->inRandomOrder()->limit(1)->first();
+        $this->assertNotNull($post);
+
         $response = $this->get("/api/posts/{$post->slug}");
         $response->assertStatus(404);
     }
 
-    public function testForGettingPostBySlug(): void
+    /** @test */
+    public function it_can_get_draft_post_by_id(): void
     {
-        $post = Post::currentStatus(Post::PUBLISH_STATUS)->first();
+        $post = Post::currentStatus(Post::DRAFT_STATUS)->inRandomOrder()->limit(1)->first();
         $this->assertNotNull($post);
-        $response = $this->get("/api/posts/{$post->slug}");
+
+        $response = $this->get("/api/posts/{$post->id}");
         $response->assertStatus(200);
+
+        $data = $response->json('data');
+        $this->assertResponseData($data);
     }
 
-    public function testForGettingNotExistingPost(): void
+    /** @test */
+    public function it_can_get_post_by_slug(): void
+    {
+        $post = Post::currentStatus(Post::PUBLISH_STATUS)->inRandomOrder()->limit(1)->first();
+        $this->assertNotNull($post);
+
+        $response = $this->get("/api/posts/{$post->slug}");
+        $response->assertStatus(200);
+
+        $data = $response->json('data');
+        $this->assertResponseData($data);
+    }
+
+    /** @test */
+    public function it_returns_404_when_getting_not_exists_post(): void
     {
         $id = Str::uuid();
         $response = $this->get("/api/posts/{$id}");
